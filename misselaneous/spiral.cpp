78e382d23579092f65dd7431d8dda5702458681c
Kishor3009 <shinde.kishor3009@gmail.com>
2024-02-14 21:40:44 +0530

    up

---
+++ misselaneous/spiral.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> spiralOrder(std::vector<std::vector<int>>& matrix) {
+        std::vector<int> ans;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int count = 0;
+        int total = row * col;
+
+        int startingRow = 0;
+        int startingCol = 0;
+        int endingRow = row - 1;
+        int endingCol = col - 1;
+
+        while (count < total) {
+            for (int index = startingCol; count < total && index <= endingCol; index++) {
+                ans.push_back(matrix[startingRow][index]);
+                count++;
+            }
+            startingRow++;
+            for (int index = startingRow; count < total && index <= endingRow; index++) {
+                ans.push_back(matrix[index][endingCol]);
+                count++;
+            }
+            endingCol--;
+            for (int index = endingCol; count < total && index >= startingCol; index--) {
+                ans.push_back(matrix[endingRow][index]);
+                count++;
+            }
+            endingRow--;
+            for (int index = endingRow; count < total && index >= startingRow; index--) {
+                ans.push_back(matrix[index][startingCol]);
+                count++;
+            }
+            startingCol++;
+        }
+        return ans;
+    }
+
+int main(){
+    vector<vector<int>> matrix = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+   
+    vector<int> result = spiralOrder(matrix);
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout<<endl;
+    return 0;
+}
