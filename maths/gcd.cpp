f36aa74aa0a6470c3c69db2f0a899f4f77a47490
Kishor3009 <shinde.kishor3009@gmail.com>
2024-02-15 18:20:49 +0530

    up

---
+++ maths/gcd.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a,int b){
+    if(a==0) return b;
+    if(b==0) return a;
+    while (a!=b)
+    {
+        if(a>b) a=a-b;
+        else b=b-a;
+    }
+    
+}
+int main(){
+    int a,b;
+    cout<<"Enter the value of a and b : "<<endl;
+    cin>>a>>b;
+    int ans=gcd(a,b);
+    cout<<"gcd is : "<<ans;
+    return 0;
+}
